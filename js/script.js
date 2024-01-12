88ea508d737781a3368c4de3c828e4ff6c16ccad
Balner123 <stepan.balner@proton.me>
2024-01-12 22:02:24 +0100

    regress_first

--- js/script.js
+++ js/script.js
@@ -3,10 +3,9 @@ let circleY;
 let centerx = [];
 let centery = [];
 let orbitals = [];
-let NT_NUMBER = 1;
+let NT_NUMBER = 4;
 let velocites = [3,-3,3,-3];
 let orbits = [200,100,50,25];
-let anglep = [0,0,0,0];
 let numberInputValue;
 
 class Orbital {
@@ -37,17 +36,10 @@ class Orbital {
   }
 
   angles() {
-    if(this.i%1){
       this.angle += velocites[this.i];
-        if (this.angle == 360) {
+        if (this.angle == 360 || this.angle == -360) {
           this.angle = 0;
         }
-    }else{
-      this.angle -= velocites[this.i];
-        if (this.angle == -360) {
-          this.angle = 0;
-        }
-    }
   }
 
   draw() {
@@ -152,7 +144,7 @@ function draw() {
 
   for(let i=0;i<NT_NUMBER;i++){
 
-    text("Orbital"+ i + " l=" + orbits[i] + " v=" + velocites[i] + ";", 30, height - 30-i*40);
+    text("Orbital"+ i + " l=" + orbits[i] + " v=" + velocites[i] + ";", 30, height -20 -i*30);
 
   }
   pop();
